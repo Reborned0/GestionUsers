0198345758feb96eb9123dd2a68a4b787f32b611
Reborned0 <bob.clement@hotmail.fr>
2019-03-26 09:46:03 +0100

    Premiere fenetre

--- src/Main_Users.java
+++ src/Main_Users.java
@@ -1,8 +1,20 @@
+import javax.swing.JFrame;
 
 public class Main_Users {
 
 	public static void main(String[] args) {
-
+		JFrame fenetre = new JFrame();
+		
+		//Nom de la fenetre
+		fenetre.setTitle("Acceuil");
+		// Taille de la fenetre 
+		fenetre.setSize(1400, 1000);
+		//Position central
+		fenetre.setLocationRelativeTo(null);
+		// Croix rouge 
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Rendue visible
+		fenetre.setVisible(true);
 	}
 
 }
