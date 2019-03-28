ff2960613f67d98dbfa16e2d75e8ea4e21102780
Reborned0 <bob.clement@hotmail.fr>
2019-03-28 16:04:20 +0100

    Ajout d'une recherche d'utilisateur

---
+++ src/Recherche_Util_App.java
@@ -0,0 +1,59 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JList;
+
+public class Recherche_Util_App extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Recherche_Util_App frame = new Recherche_Util_App();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Recherche_Util_App() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1000, 674);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setText("");
+		textField.setBounds(242, 97, 370, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnRecherche = new JButton("Recherche");
+		btnRecherche.setBounds(647, 96, 89, 23);
+		contentPane.add(btnRecherche);
+		
+		JList list = new JList();
+		list.setBounds(242, 151, 494, 362);
+		contentPane.add(list);
+	}
+}
